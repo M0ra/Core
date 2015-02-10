4c35e06c6aae8250d5ebbde6c04093d89e5235f0
Alex <proakwer@gmail.com>
2015-02-10 18:38:43 +0300

    ...

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_grand_champions.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/boss_grand_champions.cpp
@@ -163,7 +163,7 @@ struct npc_mounted_championAI : ScriptedAI
             me->Dismount();
             me->RemoveAurasDueToSpell(SPELL_SHIELD);
             me->CastSpell(me, SPELL_KNEE, true);
-            me->SetTarget(0);
+            me->SetTarget(target->GetGUID());
             me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
         }
     }
@@ -203,7 +203,7 @@ struct npc_mounted_championAI : ScriptedAI
 
     bool isInMountedGauntlet()
     {
-        if(Creature* announcer = me->GetCreature(*me, instance->GetGuidData(DATA_ANNOUNCER)))
+        if(Creature* announcer = ObjectAccessor::GetCreature(*me, instance->GetGuidData(DATA_ANNOUNCER)))
         {
             if (announcer->AI()->GetData(EVENT_INTRO) == IN_PROGRESS || announcer->AI()->GetData(EVENT_WAVES) == IN_PROGRESS)
                 return true;
@@ -327,7 +327,7 @@ public:
                 defeated = true;
                 me->SetUnitMovementFlags(MOVEMENTFLAG_WALKING);
                 me->GetMotionMaster()->MovePoint(1, me->GetHomePosition());
-                me->SetTarget(0);
+                me->SetTarget(target->GetGUID());
                 me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
                 BindPlayersToInstance(me);
             }
@@ -488,7 +488,7 @@ public:
                 defeated = true;
                 me->SetUnitMovementFlags(MOVEMENTFLAG_WALKING);
                 me->GetMotionMaster()->MovePoint(1, me->GetHomePosition());
-                me->SetTarget(0);
+                me->SetTarget(target->GetGUID());
                 me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
                 BindPlayersToInstance(me);
             }
@@ -675,7 +675,7 @@ public:
                 defeated = true;
                 me->SetUnitMovementFlags(MOVEMENTFLAG_WALKING);
                 me->GetMotionMaster()->MovePoint(1, me->GetHomePosition());
-                me->SetTarget(0);
+                me->SetTarget(target->GetGUID());
                 me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
                 BindPlayersToInstance(me);
             }
@@ -835,7 +835,7 @@ public:
                 defeated = true;
                 me->SetUnitMovementFlags(MOVEMENTFLAG_WALKING);
                 me->GetMotionMaster()->MovePoint(1, me->GetHomePosition());
-                me->SetTarget(0);
+                me->SetTarget(target->GetGUID());
                 me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
                 BindPlayersToInstance(me);
             }
@@ -1022,7 +1022,7 @@ public:
                 defeated = true;
                 me->SetUnitMovementFlags(MOVEMENTFLAG_WALKING);
                 me->GetMotionMaster()->MovePoint(1, me->GetHomePosition());
-                me->SetTarget(0);
+                me->SetTarget(target->GetGUID());
                 me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
                 BindPlayersToInstance(me);
             }
