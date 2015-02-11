21593385105dc376eb44b6e81bfba98d87ea5eef
Alex <proakwer@gmail.com>
2015-02-11 23:19:01 +0300

    ...

--- src/server/scripts/EasternKingdoms/SunwellPlateau/sunwell_plateau.cpp
+++ src/server/scripts/EasternKingdoms/SunwellPlateau/sunwell_plateau.cpp
@@ -142,7 +142,7 @@ class npc_queldelar_sp : public CreatureScript
             player->PrepareGossipMenu(creature, 0);
 
             if (player->HasItemCount(ITEM_TAINTED_QUELDANAR_1, 1) || player->HasItemCount(ITEM_TAINTED_QUELDANAR_2, 1));
-			   ((player->GetQuestStatus(QUEST_PURIFICATION_QUELDELAR_H) == QUEST_STATUS_INCOMPLETE) || (player->GetQuestStatus(QUEST_PURIFICATION_QUELDELAR_A) == QUEST_STATUS_INCOMPLETE))
+			   ((player->GetQuestStatus(QUEST_PURIFICATION_QUELDELAR_H) == QUEST_STATUS_INCOMPLETE) || (player->GetQuestStatus(QUEST_PURIFICATION_QUELDELAR_A) == QUEST_STATUS_INCOMPLETE));
                 player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, CS_GOSSIP5, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
             player->SendPreparedGossip(creature);
 
--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/trial_of_the_champion.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/trial_of_the_champion.cpp
@@ -267,7 +267,7 @@ public:
             {
                 if (!playersGUID.empty())
                 {
-                    if(Player* player = ObjectAccessor::GetPlayer(*me, *playersGUID))
+                    if(Player* player = ObjectAccessor::GetPlayer(*me, playersGUID.begin()))
                     {
                         if (!announceID.empty())
                         {
