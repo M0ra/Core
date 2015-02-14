5e81a4c3faee6671788393913e494d8df30eecb7
Alex <proakwer@gmail.com>
2015-02-14 19:29:49 +0300

    revert

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1418,8 +1418,6 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
 
-void AddSC_PWS_Transmogrification();
-void AddSC_CS_Transmogrification();
 // 3
 // 4
 // 5
@@ -1465,8 +1463,6 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
 
-    AddSC_PWS_Transmogrification();
-    AddSC_CS_Transmogrification();
     // 3
     // 4
     // 5
