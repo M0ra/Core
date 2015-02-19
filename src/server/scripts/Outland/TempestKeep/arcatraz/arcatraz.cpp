35eea02284d9365d677e664025fa359e1eedb479
Alex <proakwer@gmail.com>
2015-02-19 06:30:56 +0300

    tabs

--- src/server/scripts/Outland/TempestKeep/arcatraz/arcatraz.cpp
+++ src/server/scripts/Outland/TempestKeep/arcatraz/arcatraz.cpp
@@ -337,8 +337,8 @@ class npc_warden_mellichar : public CreatureScript
                 instance->HandleGameObject(instance->GetGuidData(DATA_WARDENS_SHIELD), false);
                 IsRunning = true;
             }
-			
-			void JustSummoned(Creature* summon) override
+            
+            void JustSummoned(Creature* summon) override
             {
                 DoZoneInCombat(summon);
                 if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 100.0f, true))
