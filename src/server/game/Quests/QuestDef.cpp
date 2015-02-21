6ac515c7094752cfe5982f639de1871f126473a2
Alex <proakwer@gmail.com>
2015-02-21 14:40:29 +0300

    IsRaidQuest

--- src/server/game/Quests/QuestDef.cpp
+++ src/server/game/Quests/QuestDef.cpp
@@ -235,6 +235,8 @@ bool Quest::IsRaidQuest(Difficulty difficulty) const
             return !(difficulty & RAID_DIFFICULTY_MASK_25MAN);
         case QUEST_TYPE_RAID_25:
             return difficulty & RAID_DIFFICULTY_MASK_25MAN;
+		case QUEST_TYPE_PVP:
+            return Flags & QUEST_TYPE_RAID;	
         default:
             break;
     }
