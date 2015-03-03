e18ed3e282e6054a43bee3afdb8c8399365b54f0
Alex <proakwer@gmail.com>
2015-03-03 19:57:15 +0300

    Fix Feral Charge and Beacon of Light Combat issues

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3007,6 +3007,9 @@ void SpellMgr::LoadSpellInfoCorrections()
                 spellInfo->Effects[EFFECT_0].TriggerSpell = 36325; // They Must Burn Bomb Drop (DND)
                 break;
             case 49838: // Stop Time
+            case 53651: // Light's Beacon (Hidden periodic aura on Beacon of Light target)
+            case 50259: // Daze from Feral Charge - Cat
+            case 49376: // Feral Charge - Cat
 			case 69438: // Sample Satisfaction
             case 69445: // Perfume Spritz
             case 69489: // Chocolate Sample
