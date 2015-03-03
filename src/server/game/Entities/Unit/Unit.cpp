472c5fbe4b1c4b8811bb6f119f92e0fd86adc8ea
Alex <proakwer@gmail.com>
2015-03-03 19:44:32 +0300

    all these spells should have only 50% bonus damage on crit like a magic spells

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10705,6 +10705,11 @@ uint32 Unit::SpellCriticalDamageBonus(SpellInfo const* spellProto, uint32 damage
 
     if (damage > uint32(crit_bonus))
     {
+       // all these spells should have only 50% bonus damage on crit like a magic spells
+        if (spellProto->Id == 55078 || spellProto->Id == 61840 ||
+            (spellProto->SpellFamilyName == SPELLFAMILY_HUNTER && spellProto->SpellFamilyFlags[0] & 0x4000))
+            crit_bonus /= 2;
+
         // adds additional damage to critBonus (from talents)
         if (Player* modOwner = GetSpellModOwner())
             modOwner->ApplySpellMod(spellProto->Id, SPELLMOD_CRIT_DAMAGE_BONUS, crit_bonus);
