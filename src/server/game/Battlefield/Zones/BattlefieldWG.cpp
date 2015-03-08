9a668fabb7a9b2500feca336269c43303adcc64e
Alex <proakwer@gmail.com>
2015-03-09 01:40:00 +0300

    Rever test WG

--- src/server/game/Battlefield/Zones/BattlefieldWG.cpp
+++ src/server/game/Battlefield/Zones/BattlefieldWG.cpp
@@ -31,13 +31,6 @@
 #include "TemporarySummon.h"
 #include "WorldSession.h"
 
-enum CreditsWintergrasp
-{
-    QUEST_VICTORY_WINTERGRASP_A         = 13181,
-    QUEST_VICTORY_WINTERGRASP_H         = 13183,
-    CREDIT_TOWERS                       = 35074
-};
-
 BattlefieldWG::~BattlefieldWG()
 {
     for (WintergraspWorkshop* workshop : Workshops)
@@ -277,9 +270,6 @@ void BattlefieldWG::OnBattleStart()
 
 void BattlefieldWG::UpdateCounterVehicle(bool init)
 {
-    if (!IsWarTime())
-        return;
-
     if (init)
     {
         SetData(BATTLEFIELD_WG_DATA_VEHICLE_H, 0);
@@ -370,18 +360,6 @@ void BattlefieldWG::OnBattleEnd(bool endByTimer)
         {
             player->CastSpell(player, SPELL_ESSENCE_OF_WINTERGRASP, true);
             player->CastSpell(player, SPELL_VICTORY_REWARD, true);
-			if (GetData(BATTLEFIELD_WG_DATA_DAMAGED_TOWER_DEF))
-                player->CastSpell(player, SPELL_DAMAGED_BUILDING, true);
-            else
-                if (!GetData(BATTLEFIELD_WG_DATA_BROKEN_TOWER_DEF))
-                    player->CastSpell(player, SPELL_INTACT_BUILDING, true);
-            if (GetData(BATTLEFIELD_WG_DATA_BROKEN_TOWER_ATT))
-                player->CastSpell(player, SPELL_DESTROYED_TOWER, true);
-            else
-                if (GetData(BATTLEFIELD_WG_DATA_DAMAGED_TOWER_ATT))
-                    player->CastSpell(player, SPELL_DAMAGED_TOWER, true);
-            player->AreaExploredOrEventHappens(QUEST_VICTORY_WINTERGRASP_A);
-            player->AreaExploredOrEventHappens(QUEST_VICTORY_WINTERGRASP_H);
             // Send Wintergrasp victory achievement
             DoCompleteOrIncrementAchievement(ACHIEVEMENTS_WIN_WG, player);
             // Award achievement for succeeding in Wintergrasp in 10 minutes or less
@@ -391,17 +369,8 @@ void BattlefieldWG::OnBattleEnd(bool endByTimer)
     }
 
     for (GuidSet::const_iterator itr = m_PlayersInWar[GetAttackerTeam()].begin(); itr != m_PlayersInWar[GetAttackerTeam()].end(); ++itr)
-	{
         if (Player* player = ObjectAccessor::FindPlayer(*itr))
-		{
             player->CastSpell(player, SPELL_DEFEAT_REWARD, true);
-			if (GetData(BATTLEFIELD_WG_DATA_BROKEN_TOWER_DEF))
-                player->CastSpell(player, SPELL_DESTROYED_TOWER, true);
-            else
-                if (GetData(BATTLEFIELD_WG_DATA_DAMAGED_TOWER_DEF))
-                    player->CastSpell(player, SPELL_DAMAGED_TOWER, true);
-        }
-    }
 
     for (uint8 team = 0; team < 2; ++team)
     {
@@ -882,16 +851,17 @@ void BattlefieldWG::SendInitWorldStatesToAll()
                 SendInitWorldStatesTo(player);
 }
 
-void BattlefieldWG::BrokenWallOrTower(TeamId team)
+void BattlefieldWG::BrokenWallOrTower(TeamId /*team*/)
 {
-    if (team == GetDefenderTeam())
+// might be some use for this in the future. old code commented out below. KL
+/*    if (team == GetDefenderTeam())
     {
-		for (GuidSet::const_iterator itr = m_PlayersInWar[GetDefenderTeam()].begin(); itr != m_PlayersInWar[GetDefenderTeam()].end(); ++itr)
+        for (GuidSet::const_iterator itr = m_PlayersInWar[GetAttackerTeam()].begin(); itr != m_PlayersInWar[GetAttackerTeam()].end(); ++itr)
         {
             if (Player* player = ObjectAccessor::FindPlayer(*itr))
-                player->KilledMonsterCredit(CREDIT_TOWERS);
+                IncrementQuest(player, WGQuest[player->GetTeamId()][2], true);
         }
-    }
+    }*/
 }
 
 // Called when a tower is broke
@@ -964,14 +934,6 @@ void BattlefieldWG::ProcessEvent(WorldObject* obj, uint32 eventId)
                     building->Damaged();
                 else if (buildingGo->GetGOInfo()->building.destroyedEvent == eventId)
                     building->Destroyed();
-					
-               // Add Support of Quests Toppling the Towers & Southern Sabotage
-                if (go->GetEntry()==190356 || go->GetEntry()==190357 || go->GetEntry()==190358)
-                {
-                    for (GuidSet::const_iterator itr = m_PlayersInWar[GetDefenderTeam()].begin(); itr != m_PlayersInWar[GetDefenderTeam()].end(); ++itr)
-                        if (Player* player = ObjectAccessor::FindPlayer(*itr))
-                            player->RewardPlayerAndGroupAtEvent(35074, go);
-                }					
                 break;
             }
         }
@@ -1194,8 +1156,7 @@ void BfWGGameObjectBuilding::Destroyed()
             break;
     }
 
-    if (_type == BATTLEFIELD_WG_OBJECTTYPE_TOWER)	
-        _wg->BrokenWallOrTower(_teamControl);
+    _wg->BrokenWallOrTower(_teamControl);
 }
 
 void BfWGGameObjectBuilding::Init(GameObject* go)
