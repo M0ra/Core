b29a49bbb9b49e71f9d583f1419831a7e38cd872
Alex <proakwer@gmail.com>
2015-03-16 22:37:42 +0300

    Fix Living Bomb

--- src/server/scripts/Spells/spell_mage.cpp
+++ src/server/scripts/Spells/spell_mage.cpp
@@ -434,7 +434,7 @@ class spell_mage_living_bomb : public SpellScriptLoader
                     return;
 
                 if (Unit* caster = GetCaster())
-                    caster->CastSpell(GetTarget(), uint32(aurEff->GetAmount()), true, NULL, aurEff);
+                    GetTarget()->CastSpell(GetTarget(), uint32(aurEff->GetAmount()), true, NULL, aurEff, caster->GetGUID());
             }
 
             void Register() override
