353e21c41475fa5210b6ef40710b42dfde1058bb
Alex <proakwer@gmail.com>
2015-04-16 18:01:19 +0300

    Update Battleground.h

--- src/server/game/Battlegrounds/Battleground.h
+++ src/server/game/Battlegrounds/Battleground.h
@@ -172,6 +172,7 @@ struct BattlegroundObjectInfo
 
 enum ArenaType
 {
+    ARENA_TYPE_1v1          = 1,
     ARENA_TYPE_2v2          = 2,
     ARENA_TYPE_3v3          = 3,
     ARENA_TYPE_5v5          = 5
