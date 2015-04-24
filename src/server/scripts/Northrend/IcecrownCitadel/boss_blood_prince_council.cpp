d3aea764b0aa0be7862160757f22256d3188d5ca
Alex <proakwer@gmail.com>
2015-04-24 15:25:04 +0300

    Update boss_blood_prince_council.cpp

--- src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
@@ -412,7 +412,7 @@ class boss_prince_keleseth_icc : public CreatureScript
                 if (IsHeroic())
                 {
                     me->AddAura(SPELL_SHADOW_PRISON, me);
-                    DoCast(me, SPELL_SHADOW_PRISON_DUMMY, true);
+                    DoCast(me, SPELL_SHADOW_PRISON_DUMMY);
                 }
             }
 
