cf664de9d3f4cfe2380460d323f270ccde6563f3
Grindelwald <137000133@qq.com>
2015-05-13 06:54:26 +0800

    Create 2015_05_13_boss_hodir.sql
    fix 10p Fast freezing model of fault

---
+++ sql/updates/world/2015_05_13_boss_hodir.sql
@@ -0,0 +1,3 @@
+-- Miscelaneous
+UPDATE `creature_template` SET `difficulty_entry_1`=33352, `flags_extra`=0 WHERE `entry`=32926;
+UPDATE `creature_template` SET `flags_extra`=0 WHERE `entry` IN (32938,33352,33353);
